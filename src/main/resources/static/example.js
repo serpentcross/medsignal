8da02fe389569a9f91f6d3f959c206432ff4825f
serpentcross <serpentcross@mail.ru>
2017-05-06 22:32:17 +0300

    test commit

---
+++ src/main/resources/static/example.js
@@ -0,0 +1,8 @@
+var exampleMethod = function() {
+
+    var List = Java.type("java.util.ArrayList");
+    var names = new List();
+    names.add("shekhar");
+    names.add("rahul");
+    console.log(names.get(0));
+};
